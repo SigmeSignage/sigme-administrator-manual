6d80204159ab203e8e447fe08940e44e7fdd6d99
MarkMiura <miura@sigme.net>
2022-08-29 17:21:15 +0900

    Initial setting for Administrator Manual.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sigme Creator Guide
+How to create Sigme Digital signage contents for creators
---
+++ template/default/template.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Sigme - Digital Signage - https://www.sigme.net
+ * Copyright (C) 2022 Sigme Project, Cloud signage Consortium
+ *
+ * This file is part of Sigme.
+ *
+ * Sigme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Sigme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Sigme.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+# Name of the product
+define('PRODUCT_NAME', 'Sigme');
+
+# Home page URL
+define('PRODUCT_HOME', 'https://www.sigme.net');
+
+# Product Support URL
+define('PRODUCT_SUPPORT_URL', 'https://community.xibo.org.uk/');
+define('PRODUCT_FAQ_URL', 'https://community.xibo.org.uk/c/faq');
+
